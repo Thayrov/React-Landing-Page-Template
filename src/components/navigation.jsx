7ee9a1d35a145e6aa9b6430733570c02a12ab796
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-09-03 13:30:52 -0400

    (rig)edit:index.html,style.css(agregoTangerine,*Playfair*)

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
-            caca
+            La Cabaña de las Flores
           </a>{" "}
         </div>
 
