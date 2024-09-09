f74d92f84f7cc5f3aa34c8ffacfefd72cb7a577c
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-09-09 09:30:55 -0300

    (rig)edit:services&data(texto en castellano)

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -5,10 +5,9 @@ export const Services = (props) => {
     <div id="services" className="text-center">
       <div className="container">
         <div className="section-title">
-          <h2>Our Services</h2>
+          <h2>Lo que Ofrecemos</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+          Delicadeza, color y vida en cada diseño floral. 🌷
           </p>
         </div>
         <div className="row">
