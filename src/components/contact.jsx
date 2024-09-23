1771b537472ad9b423fcc94cb2a120aaf3151dfe
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-09-22 21:00:12 -0300

    (rig)edit:Contact(/**/ code de form)

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,40 +1,41 @@
-import { useState } from "react";
-import emailjs from "emailjs-com";
+// import { useState } from "react";
+// import emailjs from "emailjs-com";
 import React from "react";
 
-const initialState = {
-  name: "",
-  email: "",
-  message: "",
-};
-export const Contact = (props) => {
-  const [{ name, email, message }, setState] = useState(initialState);
+// const initialState = {
+//   name: "",
+//   email: "",
+//   message: "",
+// };
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setState((prevState) => ({ ...prevState, [name]: value }));
-  };
-  const clearState = () => setState({ ...initialState });
-  
-  
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(name, email, message);
-    
-    /* replace below with your own Service ID, Template ID and Public Key from your EmailJS account */
-    
-    emailjs
-      .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", e.target, "YOUR_PUBLIC_KEY")
-      .then(
-        (result) => {
-          console.log(result.text);
-          clearState();
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-  };
+export const Contact = (props) => {
+  // const [{ name, email, message }, setState] = useState(initialState);
+
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setState((prevState) => ({ ...prevState, [name]: value }));
+  // };
+
+  // const clearState = () => setState({ ...initialState });
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log(name, email, message);
+
+  //   /* replace below with your own Service ID, Template ID and Public Key from your EmailJS account */
+  //   emailjs
+  //     .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", e.target, "YOUR_PUBLIC_KEY")
+  //     .then(
+  //       (result) => {
+  //         console.log(result.text);
+  //         clearState();
+  //       },
+  //       (error) => {
+  //         console.log(error.text);
+  //       }
+  //     );
+  // };
+
   return (
     <div>
       <div id="contact">
@@ -44,10 +45,11 @@ export const Contact = (props) => {
               <div className="section-title">
                 <h2>Contacto</h2>
                 <p>
-                  Por favor, complete el siguiente formulario para comunicarse con nosotros.
+                  {/* Comentado formulario de contacto */}
+                  {/* Por favor, complete el siguiente formulario para comunicarse con nosotros. */}
                 </p>
               </div>
-              <form name="sentMessage" validate onSubmit={handleSubmit}>
+              {/* <form name="sentMessage" validate onSubmit={handleSubmit}>
                 <div className="row">
                   <div className="col-md-6">
                     <div className="form-group">
@@ -94,7 +96,7 @@ export const Contact = (props) => {
                 <button type="submit" className="btn btn-custom btn-lg">
                   Enviar
                 </button>
-              </form>
+              </form> */}
             </div>
           </div>
           <div className="col-md-3 col-md-offset-1 contact-info">
@@ -151,9 +153,7 @@ export const Contact = (props) => {
       </div>
       <div id="footer">
         <div className="container text-center">
-          <p>
-            &copy; 2024 Floristería Cabaña de las Flores.
-          </p>
+          <p>&copy; 2024 Floristería Cabaña de las Flores.</p>
         </div>
       </div>
     </div>
