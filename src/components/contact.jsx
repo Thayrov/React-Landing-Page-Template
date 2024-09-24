e8af6e00838540ca04b42f63e6521f16d2ee1f6b
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-09-23 23:50:58 -0300

    (rig)edit:Contact(delete código comentado)

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,42 +1,7 @@
-// import { useState } from "react";
-// import emailjs from "emailjs-com";
 import React from "react";
 import { MapCab } from "./MapCab";
 
-// const initialState = {
-//   name: "",
-//   email: "",
-//   message: "",
-// };
-
 export const Contact = (props) => {
-  // const [{ name, email, message }, setState] = useState(initialState);
-
-  // const handleChange = (e) => {
-  //   const { name, value } = e.target;
-  //   setState((prevState) => ({ ...prevState, [name]: value }));
-  // };
-
-  // const clearState = () => setState({ ...initialState });
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   console.log(name, email, message);
-
-  //   /* replace below with your own Service ID, Template ID and Public Key from your EmailJS account */
-  //   emailjs
-  //     .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", e.target, "YOUR_PUBLIC_KEY")
-  //     .then(
-  //       (result) => {
-  //         console.log(result.text);
-  //         clearState();
-  //       },
-  //       (error) => {
-  //         console.log(error.text);
-  //       }
-  //     );
-  // };
-
   return (
     <div>
       <div id="contact">
@@ -45,59 +10,12 @@ export const Contact = (props) => {
             <div className="row">
               <div className="section-title">
                 <h2>Contacto</h2>
+                <p>
+                  Acá va a ir un comentario importante.
+                </p>
+                {/* Reemplazamos el formulario por el mapa */}
                 <MapCab />
-                {/*<p>
-                  Por favor, complete el siguiente formulario para comunicarse con nosotros.
-                </p>*/}
               </div>
-              {/* <form name="sentMessage" validate onSubmit={handleSubmit}>
-                <div className="row">
-                  <div className="col-md-6">
-                    <div className="form-group">
-                      <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        className="form-control"
-                        placeholder="Nombre"
-                        required
-                        onChange={handleChange}
-                      />
-                      <p className="help-block text-danger"></p>
-                    </div>
-                  </div>
-                  <div className="col-md-6">
-                    <div className="form-group">
-                      <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        className="form-control"
-                        placeholder="Correo"
-                        required
-                        onChange={handleChange}
-                      />
-                      <p className="help-block text-danger"></p>
-                    </div>
-                  </div>
-                </div>
-                <div className="form-group">
-                  <textarea
-                    name="message"
-                    id="message"
-                    className="form-control"
-                    rows="4"
-                    placeholder="Mensaje"
-                    required
-                    onChange={handleChange}
-                  ></textarea>
-                  <p className="help-block text-danger"></p>
-                </div>
-                <div id="success"></div>
-                <button type="submit" className="btn btn-custom btn-lg">
-                  Enviar
-                </button>
-              </form> */}
             </div>
           </div>
           <div className="col-md-3 col-md-offset-1 contact-info">
@@ -118,14 +36,6 @@ export const Contact = (props) => {
                 {props.data ? props.data.phone : "loading"}
               </p>
             </div>
-           {/* <div className="contact-item">
-              <p>
-                <span>
-                  <i className="fa fa-envelope-o"></i> Correo
-                </span>{" "}
-                {props.data ? props.data.email : "loading"}
-              </p>
-            </div>*/}
           </div>
           <div className="col-md-12">
             <div className="row">
@@ -136,16 +46,6 @@ export const Contact = (props) => {
                       <i className="fa fa-facebook"></i>
                     </a>
                   </li>
-                  {/*<li>
-                    <a href={props.data ? props.data.twitter : "/"}>
-                      <i className="fa fa-twitter"></i>
-                    </a>
-                  </li>*/}
-                  {/*<li>
-                    <a href={props.data ? props.data.youtube : "/"}>
-                      <i className="fa fa-youtube"></i>
-                    </a>
-                  </li>*/}
                 </ul>
               </div>
             </div>
